330a97f14e570015642dad450149f45ee7b53fa6
Jesuit Student 26317 <26317@jcpstudents.org>
2024-11-19 17:45:39 -0600

    Add motors

--- src/main/java/frc/robot/subsystems/Motors.java
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -4,11 +4,22 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Motors extends SubsystemBase {
-  public Motors() {}
+  
+  private final CANSparkMax motorL1, motorL2, motorR1, motorR2;
+
+  public Motors() {
+    motorL1 = new CANSparkMax(0, CANSparkLowLevel.MotorType.kBrushless);
+    motorL2 = new CANSparkMax(1, CANSparkLowLevel.MotorType.kBrushless);  
+    motorR1 = new CANSparkMax(2, CANSparkLowLevel.MotorType.kBrushless);
+    motorR2 = new CANSparkMax(3, CANSparkLowLevel.MotorType.kBrushless);
+  }
 
   @Override
   public void periodic() {
