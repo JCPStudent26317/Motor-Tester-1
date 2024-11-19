d18bb0c7b7b98280ec99aee0c156c4a6a84d086d
Jesuit Student 26317 <26317@jcpstudents.org>
2024-11-19 17:42:49 -0600

    Create Motors class

--- src/main/java/frc/robot/subsystems/Motors.java
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -7,8 +7,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  public ExampleSubsystem() {}
+public class Motors extends SubsystemBase {
+  public Motors() {}
 
   @Override
   public void periodic() {
